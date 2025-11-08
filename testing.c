d45468d5e453aaf3a5ac88a6186992058b6a15d5
Aditya Jha <78aaditya@gmail.com>
2025-11-08 20:29:26 +0530

    git pushing using vs code terminal

--- testing.c
+++ testing.c
@@ -5,5 +5,6 @@ int main(){
     printf("This will be my first git commit");
     //adding new commit message
      //adding new changes to commit
+     //adding more changes to try out new commands
     return 0;
 }
